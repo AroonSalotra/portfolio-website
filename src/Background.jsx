24913e69576c62a9d6dec324c864beb8cffbcb93
Aroon Salotra <aroonsalotra@gmail.com>
2022-07-13 21:28:39 +0100

    project initialisation

---
+++ src/Background.jsx
@@ -0,0 +1,11 @@
+const Background = () => {
+    return (
+        <div className="background-container">
+            {/* <div className="bg-square" /> */}
+            <div className="background-b"/>
+            {/* <div className="background-a"/> */}
+        </div>
+    );
+}
+
+export default Background;
---
+++ src/Intro.jsx
@@ -0,0 +1,14 @@
+import Skills from "./Skills";
+
+const Intro = () => {
+    return (
+        <div className="intro-container">
+            <div className="intro-content">
+                <h1>Aroon Salotra</h1>
+                <p>I am a self-taught software developer</p>
+            </div>
+        </div>
+    );
+}
+
+export default Intro;
