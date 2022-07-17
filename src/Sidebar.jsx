3a3982e0184d3ceb0c771287ab95949ea2c8e681
Aroon Salotra <aroonsalotra@gmail.com>
2022-07-17 02:37:37 +0100

    removed inline styles, all styles now in css

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -2,46 +2,41 @@ import { useState } from "react";
 import { BsBoxArrowLeft, BsBoxArrowRight } from "react-icons/bs"
 
 const Sidebar = () => {
-    const [activeClass, setActiveClass] = useState("sidebar")
-    const [activeClass2, setActiveClass2] = useState("hover-color cursor-pointer")
-    const [rotate, setRotate] = useState({ transform: "rotate(180deg)" })
+    const [display, setDisplay] = useState("sidebar")
+    const [hover, setHover] = useState("hover-color cursor-pointer")
+    const [rotate, setRotate] = useState("rotate-180 icon-sidebar")
 
     const handleClick = () => {
 
-        if (activeClass === "sidebar") {
-            setActiveClass("onscreen sidebar")
-            setActiveClass2("")
-            setRotate({ transform: "rotate(0deg)" })
+        if (display === "sidebar") {
+            setDisplay("onscreen sidebar")
+            setHover("")
+            setRotate("icon-sidebar")
         } else {
-            setActiveClass("sidebar")
-            setActiveClass2("hover-color cursor-pointer")
-            setRotate({ transform: "rotate(180deg)" })
-
+            setDisplay("sidebar")
+            setHover("hover-color cursor-pointer")
+            setRotate("rotate-180 icon-sidebar")
         }
     }
 
     const listContent = [
-        { text: "About", hasClass: (activeClass2) },
-        { text: "Contact", hasClass: (activeClass2) },
-        { text: "Work", hasClass: (activeClass2) },
-        { text: "Resume", hasClass: (activeClass2) },
-        { text: <BsBoxArrowRight />, hasClass: "icon-sidebar", hasStyle: (rotate), clickEffect: () => handleClick() },
+        { text: "About", hasClass: (hover) },
+        { text: "Contact", hasClass: (hover) },
+        { text: "Work", hasClass: (hover) },
+        { text: "Resume", hasClass: (hover) },
+        { text: <BsBoxArrowRight />, hasClass: (rotate), clickEffect: () => handleClick() },
     ]
 
-
-
-
-
     return (
-        <div className={activeClass}>
-            <ul>
-                {listContent.map(({ text, clickEffect, hasClass, hasStyle }) =>
-                    <li key={text} onClick={clickEffect ? clickEffect : null} style={hasStyle ? hasStyle : null}
-                        className={hasClass ? hasClass : null}>{text}</li>
-                )}
-            </ul>
-
-            {/* <div style={{ height: "2rem", width: "2rem", background: "red", marginLeft: "2.5rem", transform: "rotate(45deg)" }} /> */}
+        <div>
+            <div className={display}>
+                <ul>
+                    {listContent.map(({ text, clickEffect, hasClass }) =>
+                        <li key={text} onClick={clickEffect ? clickEffect : null}
+                            className={hasClass ? hasClass : null}>{text}</li>
+                    )}
+                </ul>
+            </div>
         </div>
     );
 }
