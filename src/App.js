366c200e5c65ad1881b1064ff528a9ff98deaa6b
Aroon Salotra <aroonsalotra@gmail.com>
2022-07-18 02:03:47 +0100

    add emailjs api on contact page

--- src/App.js
+++ src/App.js
@@ -9,18 +9,60 @@ import Work from './Work';
 import ContactForm from './ContactForm';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import { send } from "emailjs-com";
+
 
 
 const App = () => {
+
+  const [toSend, setToSend] = useState({
+    from_name: "",
+    email: "",
+    message: "",
+    reply_to: ""
+  })
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    send(
+      //ServiceID
+      'service_r76eupk',
+      //TemplateID
+      'template_u9xaust',
+      toSend,
+      //UserID
+      'mxTTQBdikoSMlFRj9'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        alert("message sent")
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+        alert("error sending message, please try again")
+      });
+
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
+
   return (
     <BrowserRouter>
 
       <Navbar />
 
       <Routes>
-        <Route exact path='/' element={<><Intro /><About/><Work/></>} />
-        <Route exact path='/contact' element={<ContactForm />} />
-        <Route exact path='/work' element={<Work/>} />
+        <Route exact path='/' element={<><Intro /><About /><Work /></>} />
+        <Route exact path='/contact' element={<ContactForm
+          toSend={toSend}
+          onSubmit={onSubmit}
+          handleChange={handleChange} />} />
+        <Route exact path='/work' element={<Work />} />
         {/* <Route exact path='/resume' element={<Resume/>} /> */}
       </Routes>
 
--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,22 +1,62 @@
-const ContactForm = () => {
+import { useState } from "react";
+import { send } from "emailjs-com";
+
+//https://medium.com/geekculture/how-to-send-emails-from-a-form-in-react-emailjs-6cdd21bb4190
+
+const ContactForm = ({toSend, onSubmit, handleChange}) => {
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [comment, setComment] = useState("")
+
+    const textChange = (e) => {
+
+        let target = "";
+
+        switch (e.target.id) {
+            case "name":
+                target = setName;
+                break;
+            case "email":
+                target = setEmail;
+                break;
+            case "comment":
+                target = setComment;
+                break;
+            default:
+                return null
+        }
+        target(e.target.value)
+    }
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+    //   alert("Submited")
+    }
+
+    console.log(toSend)
+
+
+
     return (
         <div className="contact-form">
             <h2>Contact Me</h2>
             {/* <p>* Required</p> */}
-            <form action="" className="form-elem" >
-                <label htmlFor=""> Name
-                    <input type="text" name="" id="" />
+            <form action="" className="form-elem" onSubmit={onSubmit} >
+                <label htmlFor=""> Name:
+                    <input type="text" name="from_name" id="name" value={toSend.from_name} onChange={handleChange} />
                 </label>
 
-                <label htmlFor=""> Email
-                    <input type="email" name="" id="" />
+                <label htmlFor=""> Email:
+                    <input type="email" name="email" id="email" value={toSend.email} onChange={handleChange} />
                 </label>
 
-                <label htmlFor=""> Comment
-                    <textarea name="" id="" cols="30" rows="10" />
+                <label htmlFor=""> Comment:
+                    <textarea name="message" id="comment" cols="30" rows="10" value={toSend.message} onChange={handleChange} />
                 </label>
-
-
+                <button className="form-btn"> 
+                    Submit
+                </button>
             </form>
         </div>
     );
